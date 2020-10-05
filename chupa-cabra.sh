1e7606a33a33359529c75ee4067097351766e810
Paulo Jerônimo <paulojeronimo@gmail.com>
2020-10-05 14:24:55 -0300

    Ajustes da variável "vento" e adições para execução de download em paralelo

--- chupa-cabra.sh
+++ chupa-cabra.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
+set -eou pipefail
 
+cd "`dirname "$0"`"
+
+echo "Environment settings:"
+set -x
+PWD=$PWD
 DATA_DIR=${DATA_DIR:-tmp/data}
+FAKE_MODE=${FAKE_MODE:-false}
+FAKE_SLEEP_TIME=${FAKE_SLEEP_TIME:-0.05}
+PARALLEL=${PARALLEL:-2}
+INITIAL_YEAR=${INITIAL_YEAR:-2000}
+FINAL_YEAR=${FINAL_YEAR:-2018}
+INITIAL_MONTH=${INITIAL_MONTH:-01}
+FINAL_MONTH=${FINAL_MONTH:-12}
+{ set +x; } 2> /dev/null
 
 final_day() {
 	local m=$1
@@ -25,52 +39,90 @@ request_and_save() {
 	local final=$3
 	local file_name=$DATA_DIR/${uf}_${month}_${year}.csv
 
-	echo "Gerando o arquivo $file_name ..."
-	curl -X POST http://queimadas.dgi.inpe.br/queimadas/sisam/v2/api/variaveis \
-		-d "uf=$uf" \
-		-d "inicio=$inicio"  \
-		-d "final=$final" \
-		-d 'horarios=0' \
-		-d 'horarios=6' \
-		-d 'horarios=12' \
-		-d 'horarios=18' \
-		-d 'municipios=-1' \
-		-d 'variaveis=conc_co' \
-		-d 'variaveis=conc_o3' \
-		-d 'variaveis=conc_no2' \
-		-d 'variaveis=conc_so2' \
-		-d 'variaveis=conc_pm' \
-		-d 'variaveis=conc_vento' \
-		-d 'variaveis=temp_ar' \
-		-d 'variaveis=umid_ar' \
-		-d 'variaveis=prec' \
-		-d 'variaveis=num_focos' 2> /dev/null > $file_name
+	echo "File $file_name is being generated ..."
+	$FAKE_MODE && {
+		sleep $FAKE_SLEEP_TIME
+	} || {
+		curl -X POST http://queimadas.dgi.inpe.br/queimadas/sisam/v2/api/variaveis \
+			-d "uf=$uf" \
+			-d "inicio=$inicio"  \
+			-d "final=$final" \
+			-d 'horarios=0' \
+			-d 'horarios=6' \
+			-d 'horarios=12' \
+			-d 'horarios=18' \
+			-d 'municipios=-1' \
+			-d 'variaveis=conc_co' \
+			-d 'variaveis=conc_o3' \
+			-d 'variaveis=conc_no2' \
+			-d 'variaveis=conc_so2' \
+			-d 'variaveis=conc_pm' \
+			-d 'variaveis=vento' \
+			-d 'variaveis=temp_ar' \
+			-d 'variaveis=umid_ar' \
+			-d 'variaveis=prec' \
+			-d 'variaveis=num_focos' 2> /dev/null > $file_name
+	}
+	echo "File $file_name generated!"
 }
 
-cd "`dirname "$0"`"
-mkdir -p "$DATA_DIR"
+function request_and_save_by_uf() {
+	local initial_date
+	local final_date
+	local uf=$1
+	local year
+	local month
 
-[ -f ufs.txt ] || cp all_ufs.txt ufs.txt
-
-if [ "$1" ]
-then
-	ufs=$1
-else
-	ufs="`cat ufs.txt`"
-fi
-
-for uf in $ufs
-do
-	for year in {2000..2018}
+	echo "Starting generation for UF $uf ..."
+	for year in `eval "echo {$INITIAL_YEAR..$FINAL_YEAR}"`
 	do
-		for month in {01..12}
+		for month in $(eval "echo {$INITIAL_MONTH..$FINAL_MONTH}")
 		do
 			initial_date="01/$month/$year"
 			final_date="$(final_day $month $year)/$month/$year"
 			request_and_save $uf $initial_date $final_date
 		done
 	done
-	# https://superuser.com/a/742034
-	7z a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on $DATA_DIR/$uf.7z $DATA_DIR/$uf*.csv
-	rm $DATA_DIR/$uf*.csv
+	$FAKE_MODE || {
+		# https://superuser.com/a/742034
+		7z a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on $DATA_DIR/$uf.7z $DATA_DIR/$uf*.csv &> /dev/null
+		echo -e "File $DATA_DIR/$uf.7z generated!"
+		rm -f $DATA_DIR/$uf*.csv
+		echo "Files $DATA_DIR/$uf*.csv removed!"
+	}
+	echo "Generation for UF $uf finished!"
+}
+
+mkdir -p "$DATA_DIR"
+
+[ -f ufs.txt ] || cp all_ufs.txt ufs.txt
+
+if [ "${1:-}" ]
+then
+	ufs=$1
+else
+	ufs="`cat ufs.txt`"
+fi
+
+echo "Start time: `date`"
+number_of_ufs=`cat ufs.txt | tr ' ' '\n' | wc -l | xargs`
+echo "Number of UFs to download information: $number_of_ufs"
+
+count=0
+total=0
+for uf in $ufs
+do
+	request_and_save_by_uf $uf &
+	pids[${count}]=$!
+	(( count+=1 ))
+	(( total+=1 ))
+	if (( count % PARALLEL == 0 )) || (( total == number_of_ufs ))
+	then
+		echo "Waiting for ${#pids[@]} concurrent executions ..."
+		wait ${pids[*]}
+		unset pids
+		count=0
+	fi
 done
+echo "Information for $total UFs was downloaded!"
+echo "End time: `date`"
