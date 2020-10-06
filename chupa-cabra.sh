7e4918267886c65dfacee9e1011b27cdea14008d
Paulo Jerônimo <paulojeronimo@gmail.com>
2020-10-06 14:43:01 -0300

    Adicionada a geração automática de log

--- chupa-cabra.sh
+++ chupa-cabra.sh
@@ -1,11 +1,42 @@
 #!/usr/bin/env bash
+# References:
+#	Ref1:
+#		- https://superuser.com/a/742034
+#	Ref2:
+#		- https://www.gnu.org/software/bash/manual/html_node/Redirections.html
+#		- https://tldp.org/LDP/abs/html/io-redirection.html
+#		- https://serverfault.com/a/103569
+#		- https://serverfault.com/a/477337
+#		- https://gist.github.com/gin1314/3697341
+#		- https://unix.stackexchange.com/questions/159513/what-are-the-shells-control-and-redirection-operators
+#	Ref3:
+#		- https://stackoverflow.com/a/2371288	
 set -eou pipefail
+BASE_DIR=`cd "$(dirname "$0")";pwd`
+INSTANCE=`date +%s`
+LOG=${LOG:-`basename "$0" .sh`.$INSTANCE.log}
 
-cd "`dirname "$0"`"
+# Check the prerequisites for the execution environment
+case "$OSTYPE" in
+	darwin*)
+		command -v gdate &> /dev/null || {
+			echo "Install gdate! (brew install coreutils)" | tee -a $LOG
+			exit 1
+		}
+		date() { gdate "$@"; }
+		;;
+esac
 
-echo "Environment settings:"
+# Ref3
+echo "Start time: $(date -d @$INSTANCE)" > $LOG
+echo "Configured settings:" >> $LOG
+# Ref2
+exec 3>&2
+exec 2>> $LOG
 set -x
-PWD=$PWD
+BASE_DIR=$BASE_DIR
+INSTANCE=$INSTANCE
+LOG=$LOG
 DATA_DIR=${DATA_DIR:-tmp/data}
 FAKE_MODE=${FAKE_MODE:-false}
 FAKE_SLEEP_TIME=${FAKE_SLEEP_TIME:-0.05}
@@ -16,6 +47,13 @@ INITIAL_MONTH=${INITIAL_MONTH:-01}
 FINAL_MONTH=${FINAL_MONTH:-12}
 GDRIVE_DIR=${GDRIVE_DIR:-~/Google\ Drive/tmp/queimadas}
 { set +x; } 2> /dev/null
+exec 2>&3
+exec 3>&-
+cat $LOG
+
+log() {
+	echo "$(date) → $@"
+}
 
 final_day() {
 	local m=$1
@@ -41,12 +79,12 @@ request_and_save() {
 	local file_name=$DATA_DIR/${uf}/${uf}_${year}_${month}.csv
 	local error_file=`mktemp`
 
-	echo "File $file_name is being generated ..."
+	log "File $file_name is being generated ..."
 	$FAKE_MODE && {
 		sleep $FAKE_SLEEP_TIME
 	} || {
 		! [ -f $file_name ] || {
-			echo "Skipping generation of $file_name (it already exists)!"
+			log "Skipping generation of $file_name (it already exists)!"
 			return
 		}
 		curl -X POST http://queimadas.dgi.inpe.br/queimadas/sisam/v2/api/variaveis \
@@ -68,16 +106,16 @@ request_and_save() {
 			-d 'variaveis=umid_ar' \
 			-d 'variaveis=prec' \
 			-d 'variaveis=num_focos' 2> $error_file > $file_name || {
-			echo "File $file_name could not be generated!"
+			log "File $file_name could not be generated!"
 			cp $error_file $file_name.error.txt
 			return
 		}
 	}
-	echo "File $file_name generated!"
+	log "File $file_name generated!"
 }
 
 sync_with_gdrive() {
-	echo -n "Synchronizing 7z files from $DATA_DIR to \"$GDRIVE_DIR\" ..."
+	echo -n "Synchronizing 7z files from $DATA_DIR to \"$GDRIVE_DIR\" ... "
 	rsync -a $DATA_DIR/*.7z "$GDRIVE_DIR"/
 	echo ok
 }
@@ -87,7 +125,7 @@ remove_files_with_zero_size() {
 	local f
 	for f in $(find $DATA_DIR/$uf -type f -size 0)
 	do
-		echo "Removing file $f because it has 0 size!"
+		log "Removing file $f because it has 0 size!"
 		rm -f $f
 	done
 }
@@ -100,7 +138,7 @@ request_and_save_by_uf() {
 	local month
 	local file_name=$DATA_DIR/$uf.7z
 
-	echo "Starting generation for UF $uf ..."
+	log "Starting generation for UF $uf ..."
 	$FAKE_MODE || {
 		mkdir -p $DATA_DIR/$uf
 		remove_files_with_zero_size $uf
@@ -108,7 +146,7 @@ request_and_save_by_uf() {
 	for year in `eval "echo {$INITIAL_YEAR..$FINAL_YEAR}"`
 	do
 		! [ -f $file_name ] || {
-			echo "Skipping generation of $file_name (it already exists)!"
+			log "Skipping generation of $file_name (it already exists)!"
 			return
 		}
 
@@ -121,16 +159,16 @@ request_and_save_by_uf() {
 	done
 	$FAKE_MODE || {
 		cd $DATA_DIR/$uf
-		# https://superuser.com/a/742034
+		# Ref1
 		7z a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on ../$(basename $file_name) *.csv &> /dev/null
 		cd - &> /dev/null
-		echo "File $file_name generated!"
+		log "File $file_name generated!"
 		cp $file_name $GDRIVE_DIR/
-		echo "File $file_name copied to \"$GDRIVE_DIR\"!"
+		log "File $file_name copied to \"$GDRIVE_DIR\"!"
 		rm -rf $DATA_DIR/$uf
-		echo "Directory $DATA_DIR/$uf removed!"
+		log "Directory $DATA_DIR/$uf removed!"
 	}
-	echo "Generation for UF $uf finished!"
+	log "Generation for UF $uf finished!"
 }
 
 mkdir -p "$DATA_DIR"
@@ -144,7 +182,8 @@ else
 	ufs="`cat ufs.txt`"
 fi
 
-echo "Start time: `date`"
+{
+cd "$BASE_DIR"
 number_of_ufs=`cat ufs.txt | tr ' ' '\n' | wc -l | xargs`
 echo "Number of UFs to download information: $number_of_ufs"
 
@@ -160,11 +199,12 @@ do
 	(( total+=1 ))
 	if (( count % PARALLEL == 0 )) || (( total == number_of_ufs ))
 	then
-		echo "Waiting for ${#pids[@]} concurrent executions ..."
+		log "Waiting for ${#pids[@]} concurrent executions ..."
 		wait ${pids[*]}
 		unset pids
 		count=0
 	fi
 done
-echo "Information for $total UFs was downloaded!"
+log "Information for $total UFs was downloaded!"
 echo "End time: `date`"
+} |& tee -a $LOG
